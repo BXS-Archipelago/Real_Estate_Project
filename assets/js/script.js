4a9594b4bc258c50312ef1202adc5dfac0c44650
BXS-Archipelago <71372294+BXS-Archipelago@users.noreply.github.com>
2022-08-30 16:19:29 +0100

    create first JS for navbar toggle

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// add event to the element:
+
+const addEventOnElement = function(element, type, listener) {
+    if (element.length > 1) {
+        for (let i = 0; i < element.length; i++) {
+            element[i].addEventListener(type, listener);
+        }
+    } else {
+        element.addEventListener(type, listener);
+    }
+}
+
+
+// Toggle Nav Bar:
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+}
+
+addEventOnElement(navToggler, "click", toggleNav);
+
+
+const closeNav = function() {
+    navbar.classList.remove("active");
+    navToggler.classList.remove("active");
+}
+
+addEventOnElement(navLinks, "click", closeNav)
